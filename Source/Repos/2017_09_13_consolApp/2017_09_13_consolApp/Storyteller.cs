24c34fc18c2bf52102ce0f51d8f6ddb76f054977
Moses Johansson <mosesjohansson@gmail.com>
2017-09-17 22:38:08 +0200

    storytelling

---
+++ Source/Repos/2017_09_13_consolApp/2017_09_13_consolApp/Storyteller.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017_09_13_consolApp
+{
+    public class Storyteller
+    {
+        public string myword = "Hello World";
+        public string TellStory(string nameOfChild, int ageOfChild, bool willBeScary)
+        {
+            string story = "";
+            string princessver = String.Format("Lyssna nu {0}. Det var en gång en prinsessa", nameOfChild);
+            string guysver = String.Format("Hallå {0}! En gång fanns en kille", nameOfChild);
+            string scaryContinued = "som egentligen var en varul";
+            string NotscaryContinued = "som tyckte om att dansa";
+            
+            if (ageOfChild < 7)
+            {
+                story =  princessver;
+            }
+            else
+            {
+                story = guysver;
+            }
+
+            if (willBeScary == true)
+            {
+                story += scaryContinued;
+            }
+            else if (willBeScary == false)
+            {
+                story += NotscaryContinued;
+            }
+
+
+            if (willBeScary == true && ageOfChild < 5)
+            {
+                story = "Unga barn ska inte bli skrämda.";
+            }
+            return story;
+        }
+
+        public void findLastCharacter()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Source/Repos/2017_09_13_consolApp/StorytellerTests/UnitTest1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _2017_09_13_consolApp;
+
+namespace StorytellerTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TellStoryToYoung()
+        {
+            // vad vi testar
+            Storyteller storyteller = new Storyteller();
+            string nameofchild = "Sara";
+            bool willbescary = false;
+            int ageOfChild = 5;
+
+            string story = storyteller.TellStory(nameofchild, ageOfChild, willbescary);
+            string princessver = String.Format("Lyssna nu {0}. Det var en gång en prinsessa", nameofchild);
+            string maturever = String.Format("Hallå {0}! En gång fanns en kille", nameofchild);
+            string scaryContinued = "som egentligen var en varul";
+            string NotscaryContinued = "som tyckte om att dansa";
+            
+            Assert.AreEqual(princessver + NotscaryContinued, story);
+        }
+        public void TellStoryToOldChildScary()
+        {
+            Storyteller storyteller = new Storyteller();
+            string nameofchild = "Peter";
+            bool willbescary = true;
+            int ageOfChild = 9;
+
+            string story = storyteller.TellStory(nameofchild, ageOfChild, willbescary);
+            string princessver = String.Format("Lyssna nu {0}. Det var en gång en prinsessa", nameofchild);
+            string maturever = String.Format("Hallå {0}! En gång fanns en kille", nameofchild);
+            string scaryContinued = "som egentligen var en varul";
+            Assert.AreEqual(maturever + scaryContinued, story);
+        }
+        [TestMethod]
+        public void TellStoryYoungAndScared()
+        {
+            Storyteller storyteller = new Storyteller();
+         
+            string story = storyteller.TellStory("Peter", 3, true);
+            string ChildrenShouldntBeScared = "Unga barn ska inte bli skrämda.";
+            Assert.AreEqual(ChildrenShouldntBeScared, story);
+        }
+        [TestMethod]
+        public void TellStoryToYoungAndNotScared()
+        {
+            Storyteller storyteller = new Storyteller();
+            string nameofchild = "Peter";
+            bool willbescary = false;
+            int ageOfChild = 3;
+            string story = storyteller.TellStory(nameofchild, ageOfChild, willbescary);
+            string princessver = String.Format("Lyssna nu {0}. Det var en gång en prinsessa", nameofchild);
+            string maturever = String.Format("Hallå {0}! En gång fanns en kille", nameofchild);
+            string NotscaryContinued = "som tyckte om att dansa";
+            
+            Assert.AreEqual(princessver + NotscaryContinued, story);
+        }
+    }
+}
