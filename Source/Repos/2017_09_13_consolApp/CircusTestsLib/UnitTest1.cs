c850647f31a8756f7a2c609821d3091f3196bd23
Moses Johansson <mosesjohansson@gmail.com>
2017-09-18 13:36:30 +0200

    another test with many performances

--- Source/Repos/2017_09_13_consolApp/CircusTestsLib/UnitTest1.cs
+++ Source/Repos/2017_09_13_consolApp/CircusTestsLib/UnitTest1.cs
@@ -46,7 +46,41 @@ namespace CircusTestsLib
             sut.ArtistPerformance(lindansare);
             sut.ArtistPerformance(firebreather);
             string expected = "Cirkusen Elefanten har 4 artister. Clownen Surskratt har uppträtt 1 gång. Linsdansaren Repo har uppträtt 2 gånger. Eldsprutare Peter har uppträtt 1 gång. Eldunderhållare Magnus har uppträtt 0 gånger.";
+            
+            // Act
+            string actual = sut.ToString();
 
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestCircusWithManyPerformancesAndFewArtists()
+        {
+            // Arrange
+            Circus sut = new Circus("Kulan");
+            Artist canon = new Artist("Kanongubben");
+
+            Artist rabbithatperson = new Artist("Mannen med stor hatt och kanin");
+            sut.Employ(canon);
+            sut.Employ(rabbithatperson);
+
+            sut.ArtistPerformance(canon);
+            sut.ArtistPerformance(canon);
+            sut.ArtistPerformance(canon);
+            sut.ArtistPerformance(canon);
+            sut.ArtistPerformance(canon);
+
+            sut.ArtistPerformance(rabbithatperson);
+
+            sut.ArtistPerformance(canon);
+            sut.ArtistPerformance(canon);
+            sut.ArtistPerformance(canon);
+            sut.ArtistPerformance(canon);
+            sut.ArtistPerformance(canon);
+            sut.ArtistPerformance(canon);
+
+            string expected = "Cirkusen Kulan har 2 artister. Kanongubben har uppträtt 11 gånger. Mannen med stor hatt och kanin har uppträtt 1 gång.";
 
             // Act
             string actual = sut.ToString();
