4dfec20cd938f96db7e6aef8a576384e41c7266a
Moses Johansson <mosesjohansson@gmail.com>
2017-10-02 10:27:19 +0200

    merge two txt files into one txt file app

---
+++ Source/Repos/2017_09_13_consolApp/ConvertToUpperCaseApp/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace ConvertToUpperCaseApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fileOneDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SaveThirdFile = new System.Windows.Forms.SaveFileDialog();
+            this.btnSelectFileOne = new System.Windows.Forms.Button();
+            this.fileTwoDialog = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnSelectFileTwo = new System.Windows.Forms.Button();
+            this.lbSelectFileOne = new System.Windows.Forms.Label();
+            this.lbSelectFileTwo = new System.Windows.Forms.Label();
+            this.btnSaveContent = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSelectFileOne
+            // 
+            this.btnSelectFileOne.Location = new System.Drawing.Point(13, 60);
+            this.btnSelectFileOne.Name = "btnSelectFileOne";
+            this.btnSelectFileOne.Size = new System.Drawing.Size(125, 23);
+            this.btnSelectFileOne.TabIndex = 0;
+            this.btnSelectFileOne.Text = "Choose file one";
+            this.btnSelectFileOne.UseVisualStyleBackColor = true;
+            this.btnSelectFileOne.Click += new System.EventHandler(this.chooseFiles_Click);
+            // 
+            // btnSelectFileTwo
+            // 
+            this.btnSelectFileTwo.Location = new System.Drawing.Point(13, 90);
+            this.btnSelectFileTwo.Name = "btnSelectFileTwo";
+            this.btnSelectFileTwo.Size = new System.Drawing.Size(125, 23);
+            this.btnSelectFileTwo.TabIndex = 1;
+            this.btnSelectFileTwo.Text = "Choose file two";
+            this.btnSelectFileTwo.UseVisualStyleBackColor = true;
+            this.btnSelectFileTwo.Click += new System.EventHandler(this.btnSelectFileTwo_Click);
+            // 
+            // lbSelectFileOne
+            // 
+            this.lbSelectFileOne.AutoSize = true;
+            this.lbSelectFileOne.Location = new System.Drawing.Point(160, 70);
+            this.lbSelectFileOne.Name = "lbSelectFileOne";
+            this.lbSelectFileOne.Size = new System.Drawing.Size(80, 13);
+            this.lbSelectFileOne.TabIndex = 2;
+            this.lbSelectFileOne.Text = "No file selected";
+            // 
+            // lbSelectFileTwo
+            // 
+            this.lbSelectFileTwo.AutoSize = true;
+            this.lbSelectFileTwo.Location = new System.Drawing.Point(160, 99);
+            this.lbSelectFileTwo.Name = "lbSelectFileTwo";
+            this.lbSelectFileTwo.Size = new System.Drawing.Size(80, 13);
+            this.lbSelectFileTwo.TabIndex = 3;
+            this.lbSelectFileTwo.Text = "No file selected";
+            // 
+            // btnSaveContent
+            // 
+            this.btnSaveContent.Location = new System.Drawing.Point(13, 120);
+            this.btnSaveContent.Name = "btnSaveContent";
+            this.btnSaveContent.Size = new System.Drawing.Size(125, 23);
+            this.btnSaveContent.TabIndex = 4;
+            this.btnSaveContent.Text = "Save contents to file";
+            this.btnSaveContent.UseVisualStyleBackColor = true;
+            this.btnSaveContent.Visible = false;
+            this.btnSaveContent.Click += new System.EventHandler(this.btnSaveContent_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnSaveContent);
+            this.Controls.Add(this.lbSelectFileTwo);
+            this.Controls.Add(this.lbSelectFileOne);
+            this.Controls.Add(this.btnSelectFileTwo);
+            this.Controls.Add(this.btnSelectFileOne);
+            this.Name = "Form1";
+            this.Text = "Text from files to file";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog fileOneDialog;
+        private System.Windows.Forms.SaveFileDialog SaveThirdFile;
+        private System.Windows.Forms.Button btnSelectFileOne;
+        private System.Windows.Forms.OpenFileDialog fileTwoDialog;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnSelectFileTwo;
+        private System.Windows.Forms.Label lbSelectFileOne;
+        private System.Windows.Forms.Label lbSelectFileTwo;
+        private System.Windows.Forms.Button btnSaveContent;
+    }
+}
+
---
+++ Source/Repos/2017_09_13_consolApp/ConvertToUpperCaseApp/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConvertToUpperCaseApp
+{
+    public partial class Form1 : Form
+    {
+        private bool FileUploadError = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void ValidateSelectUpload(OpenFileDialog openfiledialog)
+        {
+            if (Path.GetExtension(openfiledialog.FileName) != "txt")
+            { 
+                this.FileUploadError = true;
+                openfiledialog.FileName = "";
+                MessageBox.Show("Filen kan endast vara txt");
+            }
+            else
+                this.FileUploadError = false;
+        }
+
+        private void chooseFiles_Click(object sender, EventArgs e)
+        {
+            fileOneDialog.ShowDialog();
+            lbSelectFileOne.Text = fileOneDialog.FileName;
+            if (fileOneDialog.FileName != "" && fileTwoDialog.FileName != "")
+                btnSaveContent.Visible = true;
+
+            ValidateSelectUpload(fileOneDialog);
+        }
+
+        private void btnSelectFileTwo_Click(object sender, EventArgs e)
+        {
+            fileTwoDialog.ShowDialog();
+            lbSelectFileTwo.Text = fileTwoDialog.FileName;
+            if(fileOneDialog.FileName != "" && fileTwoDialog.FileName != "")
+            btnSaveContent.Visible = true;
+
+            ValidateSelectUpload(fileTwoDialog);
+        }
+
+        private void btnSaveContent_Click(object sender, EventArgs e)
+        {
+            if(this.FileUploadError == false)
+            { 
+                SaveThirdFile.ShowDialog();
+
+                if (File.Exists(fileOneDialog.FileName) && File.Exists(fileTwoDialog.FileName))
+                {
+                    try
+                    {
+                        string contents = File.ReadAllText(fileOneDialog.FileName) + File.ReadAllText(fileTwoDialog.FileName);
+                        File.AppendAllText(SaveThirdFile.FileName, contents);
+                        MessageBox.Show("Content made into one file txt file.");
+                    }
+                    catch (IOException ioe)
+                    {
+                        MessageBox.Show(ioe.StackTrace);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Error. Please make sure you have selected two txt files");
+                }
+            }
+        }
+    }
+}
